600d19e850768a4ea190cd04d4e749df52a7e2c8
porphyriacus <dashasarantseva2007@gmail.com>
2026-07-21 20:33:26 +0300

    Application: add errors for Result pattern

---
+++ ECommerce.Application/Features/Common/Behaviors/Errors/Error.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Application.Features.Common.Behaviors.Errors
+{
+    public record Error
+    {
+        public string Code { get; }
+        public string? Message { get; }
+
+        public ErrorType Type { get; }
+
+        protected Error(string code, string? message, ErrorType errorType)
+        {
+            Code = code;
+            Message = message;
+            Type = errorType;
+        }
+
+        public static Error None => new(string.Empty, null, ErrorType.None);
+        public static Error Failure(string code, string? message = null) => 
+                                                new(code, message, ErrorType.Failure);
+        public static Error NotFound(string code, string? message) => new(code, message, ErrorType.NotFound);
+        public static Error Validation(string code, string? message) => new(code, message, ErrorType.Validation);
+        public static Error Conflict(string code, string? message) => new(code, message, ErrorType.Conflict);
+        public static Error Unauthorized(string code, string? message) => new(code, message, ErrorType.Unauthorized);
+    }
+}
---
+++ ECommerce.Application/Features/Common/Behaviors/Errors/ErrorType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Application.Features.Common.Behaviors.Errors
+{
+    public enum ErrorType
+    {
+        None,
+        Failure,       
+        NotFound,      
+        Validation,    
+        Conflict,       
+        Unauthorized    
+    }
+}
