7180d694f402fb7b61883d79518ad8b7d449c3b3
porphyriacus <dashasarantseva2007@gmail.com>
2026-07-21 21:05:59 +0300

    Application: add Result+Result<T> for Result pattern

---
+++ ECommerce.Application/Features/Common/Models/Result.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Application.Features.Common.Behaviors.Errors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Application.Features.Common.Models
+{
+    public class Result
+    {
+        public bool IsSuccess { get; }
+        public bool IsFailure => !IsSuccess;
+
+        public Error Error { get; }
+
+
+        protected Result(bool isSuccess, Error error) 
+        {
+
+            if (isSuccess && error != Error.None)
+                throw new InvalidOperationException("Successful result cannot have an error");
+
+            if (!isSuccess && error == Error.None)
+                throw new InvalidOperationException("Failed result must have an error");
+
+            IsSuccess = isSuccess;
+            Error = error;
+        }
+
+        public static Result Ok => new(true,  Error.None);
+        public static Result Failure(Error error) => new(false, error);
+
+        public static implicit operator Result(Error error) => Failure(error);
+    }
+}
---
+++ ECommerce.Application/Features/Common/Models/ResultT.cs
@@ -0,0 +1,32 @@
+﻿using ECommerce.Application.Features.Common.Behaviors.Errors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Application.Features.Common.Models
+{
+    public class Result<T> : Result
+    {
+        private readonly T _value;
+
+        private Result(T value) : base(true, Error.None)
+        {
+            _value = value;
+        }
+
+        private Result(Error error) : base(false, error)
+        {
+            _value = default;
+        }
+
+        public T Value => IsSuccess
+            ? _value!
+            : throw new InvalidOperationException("Value cannot be accessed when IsFailure");
+
+        public static Result<T> Ok(T value) => new(value);
+        public static Result<T> Failure(Error error) => new(error);
+
+        public static implicit operator Result<T>(Error error) => Failure(error);
+        public static implicit operator Result<T>(T value) => Ok(value);
+    }
+}
