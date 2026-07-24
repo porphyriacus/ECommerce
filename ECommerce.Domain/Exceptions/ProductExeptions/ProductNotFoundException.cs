9f80231802dfdf9929ad7f1e757e9ee0944f82e9
porphyriacus <porph@example.com>
2026-07-24 10:08:46 +0300

    add exept

---
+++ ECommerce.Domain/Exceptions/ProductExeptions/ProductNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Domain.Exceptions.Product
+{
+    public class ProductNotFoundException : DomainException
+    {
+        public Guid ProductId { get; }
+
+        public ProductNotFoundException(Guid productId)
+            : base($"Product with ID {productId} was not found")
+        {
+            ProductId = productId;
+        }
+    }
+}
